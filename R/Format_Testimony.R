6f0590dc055d6c723d041b80f782203b3bf128ee
rachelrogers-gbr <rachel.rogers@huskers.unl.edu>
2024-05-14 13:28:55 -0500

    creating testimony formatting function

---
+++ R/Format_Testimony.R
@@ -0,0 +1,51 @@
+#' Format Testimony
+#'
+#' @param testimony_file_path path to testimony csv file
+#'
+#' @return a formatted csv file with appropriate div classes for speech bubbles
+#' @export
+#'
+#' @examples
+
+Format_Testimony <- function(testimony_file_path){
+  `%>%` <- magrittr::`%>%`
+
+combined_testimony <- utils::read.csv(testimony_file_path)
+
+combined_testimony$before <- NA
+combined_testimony$after <- NA
+
+right_testimony <- combined_testimony %>% dplyr::filter(Bubble == "Right")
+
+right_testimony$before = paste0("<div class='speech-bubble ",right_testimony$Speaker,
+                                "'><div class='left-text'>")
+right_testimony$after = paste0("</div><div class='",right_testimony$Speaker,"-image'></div></div><br/>")
+
+center_left_testimony <- combined_testimony %>% dplyr::filter(Bubble %in% c("Center", "Left"))
+
+center_left_testimony$before = paste0("<div class='speech-bubble ",
+                         center_left_testimony$Speaker,"'><div class='",
+                         center_left_testimony$Speaker,"-image'></div><div class='right-text'>")
+center_left_testimony$after = "</div></div> "
+
+narrator_testimony <- combined_testimony %>% dplyr::filter(Bubble == "None") %>%
+  dplyr::mutate(before ="", after = "")
+
+combined_testimony <- dplyr::left_join(combined_testimony, narrator_testimony)
+combined_testimony <- dplyr::left_join(combined_testimony, center_left_testimony)
+combined_testimony <- dplyr::left_join(combined_testimony, right_testimony)
+
+min_max <- combined_testimony %>% dplyr::group_by(Page) %>%
+  dplyr::summarise(min_count=min(Count), max_count=max(Count)) %>% dplyr::ungroup()
+
+combined_testimony_trial <- left_join(combined_testimony,min_max)
+
+combined_testimony_trial <- combined_testimony_trial %>%
+  dplyr::mutate(before=ifelse(Count==min_count,paste("<div style='display:grid'>",before,sep=""), before)) %>%
+  dplyr::mutate(after=ifelse(Count==max_count,paste(after,"</div>",sep=""), after))
+
+combined_testimony_trial$combined <- paste(combined_testimony_trial$before, combined_testimony_trial$Text, sep=" ")
+combined_testimony_trial$combined <- paste(combined_testimony_trial$combined, combined_testimony_trial$after, sep=" ")
+
+return(combined_testimony_trial)
+}
