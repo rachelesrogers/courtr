6f6e3a714ba89ba5802db06fab73fbf28a5f2cbc
rachelrogers-gbr <rachel.rogers@huskers.unl.edu>
2024-07-03 10:54:08 -0500

    documenting data set

---
+++ R/data.R
@@ -0,0 +1,14 @@
+#' Information on Figure Characteristics and Default Colors
+#'
+#'
+#' @format ## `figure_information`
+#' A data frame with 92 rows and 4 columns:
+#' \describe{
+#'   \item{Part}{part shown in svg - head or clothes}
+#'   \item{Label}{label shown in shiny app dropdown menu}
+#'   \item{Item}{object whose color is specified}
+#'   \item{Color}{color of Item specified}
+#'   ...
+#' }
+#' @source created from the default cartoons
+"figure_information"
